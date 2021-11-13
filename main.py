8ad1ac16d146d7206ae66d25c28e39303d2910fa
Богдан <muratovbogdan4@gmail.com>
2021-11-13 15:06:18 +0500

    Autorize added

---
+++ main.py
@@ -0,0 +1,9 @@
+log = input("Введите логин: ")
+pas = input("Введите пароль: ")
+while True:
+    if log != "Murat.bg" or pas != "12234456good":
+        print("Неверный логин или пароль!")
+        log = input("Введите логин: ")
+        pas = input("Введите пароль: ")
+    print("Добро пожаловать, Богдан")
+    break
