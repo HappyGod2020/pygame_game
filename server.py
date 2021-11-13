a8f544d775ac10d3733413aaa6fb700031634cd4
Богдан <muratovbogdan4@gmail.com>
2021-11-13 15:18:28 +0500

    Server added

--- server.py
+++ server.py
@@ -7,11 +7,13 @@ sock.bind(('', 8080))
 sock.listen(1)
 
 conn, addr = sock.accept()
+print(addr)
 
 while True:
     data = conn.recv(5120)
+    print(data)
     if not data:
         break
-    conn.send(data.upper())
+
 
 conn.close()
