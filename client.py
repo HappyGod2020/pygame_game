133ec891a6ba7cad4eb99b45d824416085e8c65f
Богдан <muratovbogdan4@gmail.com>
2021-11-14 17:01:15 +0500

    Settings(easy) added

--- client.py
+++ client.py
@@ -1,14 +1,14 @@
+
+
 import socket
 
 sock = socket.socket()
 sock.connect(('localhost', 8080))
-s = input("Введите сообщение: ")
-sock.send(bytes(s, encoding='utf-8'))
+s = input("Введите код команды или наберите сообщение: ")
+s = s.encode('utf-8')
+sock.send(s)
 
-data = sock.recv(5120)
+data = sock.recv(1024)
 sock.close()
 
-print(data)
-
-
-bytes(s, encoding='utf-8')
+print (data)
--- server.py
+++ server.py
@@ -1,19 +1,22 @@
+
 import socket
 
 sock = socket.socket()
-
 sock.bind(('', 8080))
-
-sock.listen(1)
-
+sock.listen(10)
 conn, addr = sock.accept()
-print(addr)
+
+print ('connected:', addr)
 
 while True:
-    data = conn.recv(5120)
-    print(data)
-    if not data:
-        break
+    data = conn.recv(10240)
+    data = data.decode('utf-8')
+    data = data.replace("'", "")
 
+
+    if not data == "Exit":
+        print(addr, data)
+    if data == "Exit":
+        break
 
 conn.close()
